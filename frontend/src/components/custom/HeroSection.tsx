069c98a416ddae15aabf3d4a5cb4487514f90285
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2024-06-12 07:10:30 +0900

    refactor: use next/image width StrapiImage

--- frontend/src/components/custom/HeroSection.tsx
+++ frontend/src/components/custom/HeroSection.tsx
@@ -1,3 +1,4 @@
+import { StrapiImage } from '@/components/custom/StrapiImage';
 import Link from 'next/link';
 
 interface Image {
@@ -24,18 +25,13 @@ interface HeroSectionProps {
 export function HeroSection({ data }: { readonly data: HeroSectionProps }) {
   const { heading, subHeading, image, link } = data;
 
-  const imageURL = 'http://localhost:1337' + image.url;
   return (
     <header className="relative h-[600px] overflow-hidden">
-      <img
+      <StrapiImage
         alt="Background"
-        className="absolute inset-0 object-cover w-full h-full"
+        className="absolute inset-0 object-cover w-full h-full aspect/16:9"
+        src={image.url}
         height={1080}
-        src={imageURL}
-        style={{
-          aspectRatio: '1920/1080',
-          objectFit: 'cover',
-        }}
         width={1920}
       />
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white bg-black bg-opacity-20">
