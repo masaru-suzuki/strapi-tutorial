bc6edba92dca9d135e06c481899fb1e618e26505
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2024-06-11 19:26:20 +0900

    mod: replace heroSection in page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { HeroSection } from '@/components/custom/HeroSection';
 import { flattenAttributes } from '@/lib/utils';
 import QueryString from 'qs';
 
@@ -35,12 +36,12 @@ async function getStrapiData(path: string) {
 
 export default async function Home() {
   const strapiData = await getStrapiData('/api/home-page');
-  const { title, description } = strapiData;
+  const { blocks } = strapiData;
 
   return (
-    <main className="container mx-auto py-6">
-      <h1 className="text-5xl font-bold">{title}</h1>
-      <p className="text-xl mt-4">{description}</p>
+    <main>
+      <HeroSection data={blocks[0]} />
     </main>
   );
+  // return <></>;
 }
