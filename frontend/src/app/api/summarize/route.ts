e18ef26e3528482117e300ca88c7ec554dce98de
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2024-06-13 22:09:21 +0900

    mod: exchange format youtube transcript format

--- frontend/src/app/api/summarize/route.ts
+++ frontend/src/app/api/summarize/route.ts
@@ -1,6 +1,19 @@
 import { NextRequest } from 'next/server';
 import { fetchTranscript } from '@/lib/youtube-transcript';
 
+function transformData(data: any[]) {
+  let text = '';
+
+  data.forEach((item) => {
+    text += item.text + ' ';
+  });
+
+  return {
+    data: data,
+    text: text.trim(),
+  };
+}
+
 export async function POST(req: NextRequest) {
   console.log('FROM OUR ROUTE HANDLER:', req.body);
   const body = await req.json();
@@ -13,11 +26,12 @@ export async function POST(req: NextRequest) {
   } catch (error) {
     console.error('Error processing request:', error);
     if (error instanceof Error)
-      return new Response(JSON.stringify({ error: error.message }));
+      return new Response(JSON.stringify({ error: error }));
     return new Response(JSON.stringify({ error: 'Unknown error' }));
   }
 
-  console.log('Transcript:', transcript);
+  const transformedData = transformData(transcript);
+  console.log('Transcript:', transformedData);
 
   try {
     return new Response(
