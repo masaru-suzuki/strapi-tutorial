31f8283ec6400f3bab9ec9b5a8e4bb97e940d2c1
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2024-06-13 22:33:21 +0900

    add: generateSummary logic

--- frontend/src/app/api/summarize/route.ts
+++ frontend/src/app/api/summarize/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest } from 'next/server';
+import { ChatOpenAI } from '@langchain/openai';
+import { PromptTemplate } from '@langchain/core/prompts';
+import { StringOutputParser } from '@langchain/core/output_parsers';
 import { fetchTranscript } from '@/lib/youtube-transcript';
-
 import { getUserMeLoader } from '@/data/services/get-user-me-loader';
 import { getAuthToken } from '@/data/services/get-token';
 
@@ -35,6 +37,50 @@ function transformData(data: any[] | Response) {
     data: data,
     text: text.trim(),
   };
+}
+
+const TEMPLATE = `
+INSTRUCTIONS:
+  For the this {text} complete the following steps.
+  Generate the title for based on the content provided
+  Summarize the following content and include 5 key topics, writing in first person using normal tone of voice.
+
+  Write a youtube video description
+    - Include heading and sections.
+    - Incorporate keywords and key takeaways
+
+  Generate bulleted list of key points and benefits
+
+  Return possible and best recommended key words
+`;
+
+async function generateSummary(content: string, template: string) {
+  const prompt = PromptTemplate.fromTemplate(template);
+
+  const model = new ChatOpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    modelName: process.env.OPENAI_MODEL ?? 'gpt-4-turbo-preview',
+    temperature: process.env.OPENAI_TEMPERATURE
+      ? parseFloat(process.env.OPENAI_TEMPERATURE)
+      : 0.7,
+    maxTokens: process.env.OPENAI_MAX_TOKENS
+      ? parseInt(process.env.OPENAI_MAX_TOKENS)
+      : 4000,
+  });
+
+  const outputParser = new StringOutputParser();
+  const chain = prompt.pipe(model).pipe(outputParser);
+
+  try {
+    const summary = await chain.invoke({ text: content });
+    return summary;
+  } catch (error) {
+    if (error instanceof Error)
+      return new Response(JSON.stringify({ error: error.message }));
+    return new Response(
+      JSON.stringify({ error: 'Failed to generate summary.' })
+    );
+  }
 }
 
 export async function POST(req: NextRequest) {
