d01d2f6c1dee68a17dc2279095abf3807e234d76
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2024-06-13 21:34:24 +0900

    add:enabled upload image file

--- frontend/src/components/forms/ProfileImageForm.tsx
+++ frontend/src/components/forms/ProfileImageForm.tsx
@@ -1,10 +1,10 @@
 'use client';
 import React from 'react';
-// import { useFormState } from "react-dom";
+import { useFormState } from 'react-dom';
 
 import { cn } from '@/lib/utils';
 
-// import { uploadProfileImageAction } from "@/data/actions/profile-actions";
+import { uploadProfileImageAction } from '@/data/actions/profile-actions';
 
 import { SubmitButton } from '@/components/custom/SubmitButton';
 import ImagePicker from '@/components/custom/ImagePicker';
@@ -31,18 +31,22 @@ export function ProfileImageForm({
   data: Readonly<ProfileImageFormProps>;
   className?: string;
 }) {
-  // const uploadProfileImageWithIdAction = uploadProfileImageAction.bind(
-  //   null,
-  //   data?.id
-  // );
+  const uploadProfileImageWithIdAction = uploadProfileImageAction.bind(
+    null,
+    data?.id
+  );
 
-  // const [formState, formAction] = useFormState(
-  //   uploadProfileImageWithIdAction,
-  //   initialState
-  // );
+  const [formState, formAction] = useFormState(
+    uploadProfileImageWithIdAction,
+    initialState
+  );
+
+  console.log('#################');
+  console.log(formState);
+  console.log('#################');
 
   return (
-    <form className={cn('space-y-4', className)}>
+    <form className={cn('space-y-4', className)} action={formAction}>
       <div className="">
         <ImagePicker
           id="image"
@@ -50,8 +54,8 @@ export function ProfileImageForm({
           label="Profile Image"
           defaultValue={data?.url || ''}
         />
-        {/* <ZodErrors error={formState.zodErrors?.image} />
-        <StrapiErrors error={formState.strapiErrors} /> */}
+        <ZodErrors error={formState.zodErrors?.image} />
+        <StrapiErrors error={formState.strapiErrors} />
       </div>
       <div className="flex justify-end">
         <SubmitButton text="Update Image" loadingText="Saving Image" />
