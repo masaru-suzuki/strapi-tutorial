74aa1a984d2c4155c4d450512b7cdeb8a088cade
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2024-06-13 23:23:12 +0900

    add: createSummaryAction in SummaryForm

--- frontend/src/components/forms/SummaryForm.tsx
+++ frontend/src/components/forms/SummaryForm.tsx
@@ -2,11 +2,13 @@
 
 import { useState } from 'react';
 import { toast } from 'sonner';
-import { cn, extractYouTubeID } from '@/lib/utils';
+import { cn } from '@/lib/utils';
 
 import { Input } from '@/components/ui/input';
 import { SubmitButton } from '@/components/custom/SubmitButton';
 import { generateSummaryService } from '@/data/services/summary-service';
+import { extractYouTubeID } from '@/lib/utils';
+import { createSummaryAction } from '@/data/actions/summary-actions';
 
 interface StrapiErrorsProps {
   message: string | null;
@@ -56,6 +58,27 @@ export function SummaryForm() {
       setError({
         ...INITIAL_STATE,
         message: summaryResponseData.error,
+        name: 'Summary Error',
+      });
+      setLoading(false);
+      return;
+    }
+
+    const payload = {
+      data: {
+        title: `Summary for video: ${processedVideoId}`,
+        videoId: processedVideoId,
+        summary: summaryResponseData.data,
+      },
+    };
+
+    try {
+      await createSummaryAction(payload);
+    } catch (error) {
+      toast.error('Error Creating Summary');
+      setError({
+        ...INITIAL_STATE,
+        message: 'Error Creating Summary',
         name: 'Summary Error',
       });
       setLoading(false);
