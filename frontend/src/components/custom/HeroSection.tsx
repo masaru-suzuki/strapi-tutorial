f4771e0f75c7ce21bc8d8c2dfe23405b31f5cbeb
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2024-06-13 16:09:42 +0900

    add: conditional link setting in hero section

--- frontend/src/components/custom/HeroSection.tsx
+++ frontend/src/components/custom/HeroSection.tsx
@@ -1,44 +1,44 @@
-import { StrapiImage } from '@/components/custom/StrapiImage';
-import { getUserMeLoader } from '@/data/services/get-user-me-loader';
 import Link from 'next/link';
+import { getUserMeLoader } from '@/data/services/get-user-me-loader';
+import { StrapiImage } from '@/components/custom/StrapiImage';
 
-interface Image {
+interface ImageProps {
   id: number;
   url: string;
-  alternativeText: string | null;
+  alternativeText: string;
 }
 
-interface Link {
+interface LinkProps {
   id: number;
   url: string;
   text: string;
 }
 
 interface HeroSectionProps {
-  id: number;
-  __component: string;
-  heading: string;
-  subHeading: string;
-  image: Image;
-  link: Link;
+  data: {
+    id: number;
+    __component: string;
+    heading: string;
+    subHeading: string;
+    image: ImageProps;
+    link: LinkProps;
+  };
 }
 
-export async function HeroSection({
-  data,
-}: {
-  readonly data: HeroSectionProps;
-}) {
-  const { heading, subHeading, image, link } = data;
+export async function HeroSection({ data }: Readonly<HeroSectionProps>) {
   const user = await getUserMeLoader();
-  const linkUrl = user.ok ? '/dashboard' : link.url;
+  const { heading, subHeading, image, link } = data;
+
+  const userLoggedIn = user.ok;
+  const linkUrl = userLoggedIn ? '/dashboard' : link.url;
 
   return (
     <header className="relative h-[600px] overflow-hidden">
       <StrapiImage
         alt="Background"
-        className="absolute inset-0 object-cover w-full h-full aspect/16:9"
-        src={image.url}
+        className="absolute inset-0 object-cover w-full h-full"
         height={1080}
+        src={image.url}
         width={1920}
       />
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white bg-black bg-opacity-20">
@@ -50,7 +50,7 @@ export async function HeroSection({
           className="mt-8 inline-flex items-center justify-center px-6 py-3 text-base font-medium text-black bg-white rounded-md shadow hover:bg-gray-100"
           href={linkUrl}
         >
-          {link.text}
+          {userLoggedIn ? 'Dashboard' : link.text}
         </Link>
       </div>
     </header>
