855bbf44832e1fc626ac2abccc5d53f13218a82d
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2024-06-13 22:07:49 +0900

    add: use transcript in summarize route handler

--- frontend/src/app/api/summarize/route.ts
+++ frontend/src/app/api/summarize/route.ts
@@ -1,7 +1,24 @@
 import { NextRequest } from 'next/server';
+import { fetchTranscript } from '@/lib/youtube-transcript';
 
 export async function POST(req: NextRequest) {
   console.log('FROM OUR ROUTE HANDLER:', req.body);
+  const body = await req.json();
+  const videoId = body.videoId;
+
+  let transcript: Awaited<ReturnType<typeof fetchTranscript>>;
+
+  try {
+    transcript = await fetchTranscript(videoId);
+  } catch (error) {
+    console.error('Error processing request:', error);
+    if (error instanceof Error)
+      return new Response(JSON.stringify({ error: error.message }));
+    return new Response(JSON.stringify({ error: 'Unknown error' }));
+  }
+
+  console.log('Transcript:', transcript);
+
   try {
     return new Response(
       JSON.stringify({ data: 'return from our handler', error: null }),
